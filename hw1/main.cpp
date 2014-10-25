04d74dcdc42c4cec5f48150718e39cd28068e1ba
tzuyu <tzuyu@HuangtydeMacBook-Pro.local>
2014-10-25 16:19:30 +0800

    Initial Commit

---
+++ hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by tzuyu on 2014/10/25.
+//  Copyright (c) 2014年 tzuyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
